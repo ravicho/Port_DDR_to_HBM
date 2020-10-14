3db821f6508c0cb993201aa1042eacf31e36dabf
ravicho <44479645+ravicho@users.noreply.github.com>
2020-10-14 14:35:54 -0600

    Removed files

--- build/HBM2/temp_dir/vadd_hw/vadd/vadd/ip/example/ipi_example.sh
+++
@@ -1,6 +0,0 @@
-# ==============================================================
-# Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2020.2 (64-bit)
-# Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
-# ==============================================================
-
-/proj/xbuilds/SWIP/2020.2_0929_2003/installs/lin64/Vivado/2020.2/bin/vivado  -notrace -mode batch -source ipi_example.tcl -tclargs xcu50-fsvh2104-2LV-e ../xilinx_com_hls_vadd_1_0.zip
