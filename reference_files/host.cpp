ae20d571ae0a7b87f89a4219d5499aac8677066a
ravicho <44479645+ravicho@users.noreply.github.com>
2020-11-25 14:52:46 -0700

    removed the split of buffer

--- reference_files/host.cpp
+++ reference_files/host.cpp
@@ -31,83 +31,83 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 int main(int argc, char** argv)
 {
-    if (argc != 5) {
-        std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
-		return EXIT_FAILURE;
-	}
+  if (argc != 5) {
+    std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    std::string binaryFile = argv[1];
-    long unsigned int input_data_size = atoi (argv[2]);
-    bool addRandom = atoi (argv[3]);
-    unsigned int krnl_loop = atoi (argv[4]);
-    if ((addRandom==1) && (krnl_loop<64)) krnl_loop=64; 
+  std::string binaryFile = argv[1];
+  bool addRandom = atoi (argv[3]);
+  unsigned int krnl_loop = atoi (argv[4]);
+  if ((addRandom==1) && (krnl_loop<64)) krnl_loop=64; 
 
-    long unsigned int total_data_size = input_data_size * 1024 *1024/4 ; // Convert to Meg
-    long unsigned int vector_size_bytes = total_data_size * sizeof(uint);
-    cl_int err;
-    unsigned fileBufSize;
-    size_t numIter = 2; 
+  long unsigned int vector_size_bytes = atoi (argv[2]) * 1024 *1024; // Converted to MB
+  long unsigned int total_data_size = vector_size_bytes/sizeof(int) ; // Convert to Meg
 
-    /* Reducing the data size for emulation mode */
-    char *xcl_mode = getenv("XCL_EMULATION_MODE");
-    if (xcl_mode != NULL) {
-    	vector_size_bytes = 1024 * sizeof(uint);
- 	addRandom=1;
-	krnl_loop=1;	
-    }
+  cl_int err;
+  unsigned fileBufSize;
+  size_t numIter = 1; 
 
-    // Allocate Memory in Host Memory
-    std::vector<int,aligned_allocator<int>> source_in1(total_data_size);
-    std::vector<int,aligned_allocator<int>> source_in2(total_data_size);
-    std::vector<int,aligned_allocator<int>> source_hw_results(total_data_size);
-    std::vector<int,aligned_allocator<int>> source_sw_results(total_data_size);
+  /* Reducing the data size for emulation mode */
+  char *xcl_mode = getenv("XCL_EMULATION_MODE");
+  if (xcl_mode != NULL) {
+     vector_size_bytes = 1024 * sizeof(uint);
+     addRandom=1;
+     krnl_loop=1;	
+  }
 
-    // Create the test data 
-    for(long unsigned int i = 0 ; i < total_data_size ; i++){
-        source_in1[i] = rand() % total_data_size;
-        source_in2[i] = rand() % total_data_size;
-        source_sw_results[i] = source_in1[i] + source_in2[i];
-        source_hw_results[i] = 0;
-    }
+  // Allocate Memory in Host Memory
+  std::vector<int,aligned_allocator<int>> source_in1(total_data_size);
+  std::vector<int,aligned_allocator<int>> source_in2(total_data_size);
+  std::vector<int,aligned_allocator<int>> source_hw_results(total_data_size);
+  std::vector<int,aligned_allocator<int>> source_sw_results(total_data_size);
 
-// OPENCL HOST CODE AREA START
+  // Create the test data 
+  for(long unsigned int i = 0 ; i < total_data_size ; i++){
+     source_in1[i] = rand() % total_data_size;
+     source_in2[i] = rand() % total_data_size;
+     source_sw_results[i] = source_in1[i] + source_in2[i];
+     source_hw_results[i] = 0;
+  }
+
+
 	
 // ------------------------------------------------------------------------------------
 // Step 1: Get All PLATFORMS, then search for Target_Platform_Vendor (CL_PLATFORM_VENDOR)
 //	   Search for Platform: Xilinx 
 // Check if the current platform matches Target_Platform_Vendor
 // ------------------------------------------------------------------------------------	
-    std::vector<cl::Device> devices = get_devices("Xilinx");
-    devices.resize(1);
-    cl::Device device = devices[0];
-            std::cout << "DEVICE " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+  std::vector<cl::Device> devices = get_devices("Xilinx");
+  devices.resize(1);
+  cl::Device device = devices[0];
+  std::cout << "DEVICE " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
 
 
 // ------------------------------------------------------------------------------------
 // Step 1: Create Context
 // ------------------------------------------------------------------------------------
-    OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
+  OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
 	
 // ------------------------------------------------------------------------------------
 // Step 1: Create Command Queue
 // ------------------------------------------------------------------------------------
-    OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+  OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
 
 // ------------------------------------------------------------------
 // Step 1: Load Binary File from disk
 // ------------------------------------------------------------------		
-    char* fileBuf = read_binary_file(binaryFile, fileBufSize);
-    cl::Program::Binaries bins{{fileBuf, fileBufSize}};
+  char* fileBuf = read_binary_file(binaryFile, fileBufSize);
+  cl::Program::Binaries bins{{fileBuf, fileBufSize}};
 	
 // -------------------------------------------------------------
 // Step 1: Create the program object from the binary and program the FPGA device with it
 // -------------------------------------------------------------	
-    OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
+  OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
 
 // -------------------------------------------------------------
 // Step 1: Create Kernels
 // -------------------------------------------------------------
-    OCL_CHECK(err, cl::Kernel krnl_vector_add(program,"vadd", &err));
+  OCL_CHECK(err, cl::Kernel krnl_vector_add(program,"vadd", &err));
 
   cl::Buffer buffer_in1[numIter];
   cl::Buffer buffer_in2[numIter];
@@ -119,30 +119,27 @@ int main(int argc, char** argv)
   std::vector<cl::Event> device_2_host_Wait;
   cl::Event host_2_device_Done, krnl_Done, device_2_host_Done;
 
-//printf("Data_size = %zu and Address Pattern is %d \n", total_data_size, addRandom);
-printf("\n Total Data of %2.3f Mbytes Written to global memory..split into chunks of %zu from host\n ", (float) vector_size_bytes/1024/1024, numIter);
-printf("\n Kernel is invoked %zu time and repeats iteself %d times \n\n", numIter, krnl_loop);
+  printf("\n Total Data of %2.3f Mbytes to be written to global memory from host\n ", (float) vector_size_bytes/1024/1024);
+  printf("\n Kernel is invoked %zu time and repeats iteself %d times \n\n", numIter, krnl_loop);
 
 
-std::cout << "Data Sent from Host duing each Kernel invocation = " <<  vector_size_bytes/(numIter*1024) << " KBytes " << std::endl;
+  double kernel_time_in_sec = 0, result = 0;
+  std::chrono::duration<double> kernel_time(0);
+  std::chrono::duration<double> kernel_time1(0);
 
-double kernel_time_in_sec = 0, result = 0;
-std::chrono::duration<double> kernel_time(0);
-std::chrono::duration<double> kernel_time1(0);
+  auto kernel_start = std::chrono::high_resolution_clock::now();
 
-auto kernel_start = std::chrono::high_resolution_clock::now();
-
-for (size_t j = 0; j < numIter; j++) {
+  for (size_t j = 0; j < numIter; j++) {
 
     OCL_CHECK(err, buffer_in1[j] = cl::Buffer(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes/numIter, &source_in1[j*total_data_size/numIter], &err));
-    OCL_CHECK(err, buffer_in2[j] = cl::Buffer(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes/numIter, &source_in2[j*total_data_size/numIter], &err));
     OCL_CHECK(err, buffer_output[j] = cl::Buffer(context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, vector_size_bytes/numIter, &source_hw_results[j*total_data_size/numIter], &err));
+    OCL_CHECK(err, buffer_in2[j] = cl::Buffer(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes/numIter, &source_in2[j*total_data_size/numIter], &err));
 
     int size = total_data_size/numIter;
 
-    OCL_CHECK(err, err = krnl_vector_add.setArg(1, buffer_in2[j]));
-    OCL_CHECK(err, err = krnl_vector_add.setArg(0, buffer_in1[j]));
     OCL_CHECK(err, err = krnl_vector_add.setArg(2, buffer_output[j]));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(0, buffer_in1[j]));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(1, buffer_in2[j]));
     OCL_CHECK(err, err = krnl_vector_add.setArg(3, size));
     OCL_CHECK(err, err = krnl_vector_add.setArg(4, krnl_loop));
     OCL_CHECK(err, err = krnl_vector_add.setArg(5, addRandom));
@@ -153,33 +150,28 @@ for (size_t j = 0; j < numIter; j++) {
 	
     OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output[j]},CL_MIGRATE_MEM_OBJECT_HOST));
  
-}
-    q.finish();
-    auto kernel_end = std::chrono::high_resolution_clock::now();
-    kernel_time = std::chrono::duration<double>(kernel_end - kernel_start);
-    kernel_time_in_sec = kernel_time.count();
+  }
+  q.finish();
+  auto kernel_end = std::chrono::high_resolution_clock::now();
+  kernel_time = std::chrono::duration<double>(kernel_end - kernel_start);
+  kernel_time_in_sec = kernel_time.count();
 
-//std::cout << "kernel_time = " << kernel_time.count() << std::endl;
-
-	
-// OPENCL HOST CODE AREA END
-
-    // Compare the results of the Device to the simulation
-    bool match = true;
-    for (size_t i = 0 ; i < total_data_size ; i++){
-        if (source_hw_results[i] != source_sw_results[i]){
-            std::cout << "Error: Result mismatch" << std::endl;
-            std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
+  // Compare the results of the Device to the simulation
+  bool match = true;
+  for (size_t i = 0 ; i < total_data_size ; i++){
+    if (source_hw_results[i] != source_sw_results[i]){
+      std::cout << "Error: Result mismatch" << std::endl;
+      std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
                 << " Device result = " << source_hw_results[i] << std::endl;
-            match = false;
-            break;
-        }
-    }
+      match = false;
+      break;
+   }
+  }
 	
 // ============================================================================
 // Step 3: Release Allocated Resources
 // ============================================================================
-    delete[] fileBuf;
+  delete[] fileBuf;
 
   // Multiplying the actual data size by 4 because four buffers are being
   // used.
@@ -190,12 +182,7 @@ for (size_t j = 0; j < numIter; j++) {
   std::cout << "kernel_time_in_sec = " << kernel_time_in_sec << std::endl;
 
   result /= kernel_time_in_sec; // to GBps
-
-
   std::cout << "Throughput Achived = " << result << " GB/s" << std::endl;
-  
-
   std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl; 
   return (match ? EXIT_SUCCESS : EXIT_FAILURE);
 }
-
